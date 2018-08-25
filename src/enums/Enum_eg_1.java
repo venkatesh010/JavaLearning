289a3c50a8aa7757b45285b33649db255251e483
venkatesh-010 <venkatesh.wagh7@gmail.com>
2018-08-25 14:21:10 +0530

    Adding enum example and notes for enum

---
+++ src/enums/Enum_eg_1.java
@@ -0,0 +1,16 @@
+package enums;
+
+enum TRUEORFALSE{
+    TRUE,
+    FALSE
+}
+
+public class Enum_eg_1 {
+
+    public static void main(String[] args) {
+        TRUEORFALSE t_or_f = TRUEORFALSE.FALSE;
+        System.out.println(t_or_f);
+    }
+
+
+}
